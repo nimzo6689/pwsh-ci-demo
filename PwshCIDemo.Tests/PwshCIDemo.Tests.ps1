98c81e12ceaaea7be8357e02e462e89825410aaf
nimzo6689 <nimzo.i.d.6689@gmail.com>
2023-08-09 09:56:39 +0900

    fix: Enable test case for 'Pluto'

--- PwshCIDemo.Tests/PwshCIDemo.Tests.ps1
+++ PwshCIDemo.Tests/PwshCIDemo.Tests.ps1
@@ -7,9 +7,9 @@ Describe 'Get-Planet' {
         $planets.Count | Should -Be 8
     }
 
-    # It 'Given Pluto as an parameter, the number of planets is zero, and a warning message is displayed.' {
-    #     $planets = Get-Planet -Name 'Pluto' -WarningVariable logging -WarningAction Continue
-    #     $planets.Count | Should -Be 0
-    #     $logging.Message | Should -Be 'In August 2006, Pluto was no longer classified as a planet.'
-    # }
+    It 'Given Pluto as an parameter, the number of planets is zero, and a warning message is displayed.' {
+        $planets = Get-Planet -Name 'Pluto' -WarningVariable logging -WarningAction Continue
+        $planets.Count | Should -Be 0
+        $logging.Message | Should -Be 'In August 2006, Pluto was no longer classified as a planet.'
+    }
 }
